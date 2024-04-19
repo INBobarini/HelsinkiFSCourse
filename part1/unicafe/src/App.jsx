e2d09989af695dced6dcc4d6fcf04469e80c6ddb
Iván Bobarini <inbobarini@gmail.com>
2024-04-19 16:44:43 -0300

    added 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,7 +11,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const average = (good*1+bad*-1)/(good+bad+neutral)
+  const positive = 100*good/(good+bad+neutral)
   return (
     <div>
       <h1>give feedback</h1>
@@ -22,8 +23,8 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-
-
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
